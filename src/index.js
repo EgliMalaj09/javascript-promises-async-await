564cc69340c3013dbf5191aaedd8f6065f625e5b
EgliMalaj09 <eglimalaj999@gmail.com>
2021-01-11 12:40:03 +0100

    Module 7

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {fetchBooks,fetchMovies,fetchWithTimeout} from './services';
+import { result } from 'lodash';
+import {fetchBooks,fetchMovies,fetchWithTimeout, asyncFetchBooks, asyncFetchMovies} from './services';
 
 const movies = require('./data/movies.json');
 
@@ -26,3 +27,33 @@ const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then(results => {
     console.log('getBooksOrMoviesPromise', results);
 });
+
+async function getBooksAndMoviesAsync(){
+    try{
+        const [books,movies] = await Promise.all([asyncFetchBooks(),asyncFetchMovies()])
+        return {books,movies};
+    }catch(error){
+        console.log('Error fetching books and movies',error);
+    }
+}
+
+async function getBooksOrMoviesAsync(){
+    try{
+        const values = await Promise.race([asyncFetchBooks(),asyncFetchMovies()])
+        return values;
+    }catch(error){
+        console.log('Error waiting for the promise race',error);
+    }
+}
+getBooksAndMoviesAsync().then(results=>{
+    console.log('movies and books',{
+        movies: results.movies,
+        books: results.books
+    })
+})
+
+getBooksOrMoviesAsync().then(results => {
+    console.log('movies OR books',{
+        results
+    })
+})
