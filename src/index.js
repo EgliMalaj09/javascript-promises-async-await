225474aa0f72d8b719acd4e45ebe8124639700eb
EgliMalaj09 <eglimalaj999@gmail.com>
2021-01-11 11:56:46 +0100

    Module 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import {fetchWithTimeout} from './services';
+
 export function fetchMovies(){
-    
+    const resolveFunction = ()=> movies;
+    fetchWithTimeout(1000).then(resolveFunction);
 }
+
+let movies = require('./data/movies.json');
+
+let moviePromise = fetchMovies();
+
+moviePromise.then((results)=>{
+    console.log(results);
+})
